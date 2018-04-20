3a4a0deff3d6183b293ab695ada53d3287af5085
Beetrootmonkey <n.adelhard@googlemail.com>
2018-04-20 21:16:42 +0200

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        transform.Translate(x, y, 0);
+    }
+}
