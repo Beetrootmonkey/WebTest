a0b3ce270a2db60821c7f5481377a2a1696dde00
Beetrootmonkey <n.adelhard@googlemail.com>
2018-04-21 13:26:11 +0200

    added stuff

---
+++ Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HexMetrics {
+
+    public static readonly float innerRadius = 0.5f;
+    public static readonly float outerRadius = innerRadius * (2 / Mathf.Sqrt(3));
+
+}
