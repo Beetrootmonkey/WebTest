4cd16c23a5cc978cc40c002983eb765b2adb25c9
Beetrootmonkey <n.adelhard@googlemail.com>
2018-04-21 14:28:49 +0200

    stuff

--- Assets/Scripts/TileOrdering.cs
+++ Assets/Scripts/TileOrdering.cs
@@ -6,12 +6,11 @@ public class TileOrdering : MonoBehaviour {
     Tile[] tiles = null;
     public int width = 6;
     public int height = 6;
-
-    public Tile tilePrefab;
     // Use this for initialization
-    void Start () {
-        //tiles = GetComponentsInChildren<Tile>();
-        //OrderTiles();
+    void Awake () {
+        tiles = FindObjectsOfType<Tile>();
+        
+        OrderTiles();
     }
 	
 	// Update is called once per frame
@@ -28,35 +27,59 @@ public class TileOrdering : MonoBehaviour {
 
         foreach(Tile t in tiles)
         {
-            float x = t.transform.position.x;
-            float y = t.transform.position.y;
-            t.transform.position = new Vector2(x, y);
+            settleTile(t);
+            placeTile(t);
         }
     }
 
-    void Awake()
-    {
-        tiles = new Tile[height * width];
+    //void Awake()
+    //{
+    //    tiles = new Tile[height * width];
 
-        for (int y = 0, i = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                CreateTile(x, y, i++);
-            }
-        }
+    //    for (int y = 0, i = 0; y < height; y++)
+    //    {
+    //        for (int x = 0; x < width; x++)
+    //        {
+    //            CreateTile(x, y, i);
+    //            settleTile(tiles[i]);
+    //            placeTile(tiles[i]);
+    //            i++;
+    //        }
+    //    }
+    //}
+
+    void settleTile(Tile tile)
+    {
+        float fx = tile.transform.position.x / 1.5f / HexMetrics.outerRadius;
+        float fy = tile.transform.position.y / 2 / HexMetrics.innerRadius + fx / 2 - fx * 0.5f;
+        int x = (int)fx;
+        int y = (int)fy;
+
+        tile.transform.position = new Vector2(x, y);
     }
 
-    void CreateTile(int x, int y, int i)
+    void placeTile(Tile tile)
     {
+        int x = (int)(tile.transform.position.x);
+        int y = (int)(tile.transform.position.y);
+
         Vector3 position;
         position.x = x * HexMetrics.outerRadius * 1.5f;
         position.y = (y + x * 0.5f - x / 2) * HexMetrics.innerRadius * 2;
         position.z = 0f;
-
-        Tile cell = tiles[i] = Instantiate<Tile>(tilePrefab);
-        cell.transform.SetParent(transform, false);
-        cell.transform.localPosition = position;
+        tile.transform.position = position;
     }
+
+    //void CreateTile(int x, int y, int i)
+    //{
+    //    Vector3 position;
+    //    position.x = x * HexMetrics.outerRadius * 1.5f;
+    //    position.y = (y + x * 0.5f - x / 2) * HexMetrics.innerRadius * 2;
+    //    position.z = 0f;
+
+    //    Tile cell = tiles[i] = Instantiate<Tile>(tilePrefab);
+    //    cell.transform.SetParent(transform, false);
+    //    cell.transform.localPosition = position;
+    //}
 
 }
