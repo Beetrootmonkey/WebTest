39534b558d62184ae4e271f82d1ffe932417805e
Beetrootmonkey <n.adelhard@googlemail.com>
2018-04-21 16:12:02 +0200

    embiggened map

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Tile : MonoBehaviour {
     private TileOverlay mouseOverlay;
     private SpriteRenderer mouseOverlaySpriteRenderer;
+    private static Tile playerGround;
 
     private Player player;
 
@@ -17,6 +18,7 @@ public class Tile : MonoBehaviour {
         }
 
         player = FindObjectOfType<Player>();
+        playerGround = this;
     }
 
     // Use this for initialization
@@ -31,7 +33,7 @@ public class Tile : MonoBehaviour {
 
     void OnMouseOver()
     {
-        if (mouseOverlaySpriteRenderer)
+        if (mouseOverlaySpriteRenderer && IsReachable() == Reachable.IN_RANGE)
         {
             mouseOverlay.transform.position = transform.position;
             mouseOverlaySpriteRenderer.enabled = true;
@@ -40,7 +42,7 @@ public class Tile : MonoBehaviour {
 
     void OnMouseExit()
     {
-        if (mouseOverlaySpriteRenderer)
+        if (mouseOverlaySpriteRenderer && IsReachable() == Reachable.IN_RANGE)
         {
             mouseOverlaySpriteRenderer.enabled = false;
         }
@@ -48,9 +50,55 @@ public class Tile : MonoBehaviour {
 
     void OnMouseDown()
     {
-        if(player)
+        if(IsReachable() == Reachable.IN_RANGE)
         {
             player.transform.position = transform.position;
+            playerGround = this;
+            mouseOverlaySpriteRenderer.enabled = false;
         }
+    }
+
+    enum Reachable
+    {
+        NOT_IN_RANGE,
+        IN_RANGE,
+        STANDING_ON
+    }
+
+    Reachable IsReachable()
+    {
+        if(!playerGround)
+        {
+            Debug.Log("Player position not registered!");
+            return Reachable.NOT_IN_RANGE;
+        }
+        Collider2D ownCollider = GetComponent<Collider2D>();
+        if (!ownCollider)
+        {
+            Debug.Log("Fatal: Collider of Tile under mouse missing!");
+            return Reachable.NOT_IN_RANGE;
+        }
+        Collider2D playerCollider = playerGround.GetComponent<Collider2D>();
+        if (!playerCollider)
+        {
+            Debug.Log("Fatal: Collider of Tile under player missing!");
+            return Reachable.NOT_IN_RANGE;
+        }
+
+        if(ownCollider == playerCollider)
+        {
+            return Reachable.STANDING_ON;
+        }
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(playerGround.transform.position, HexMetrics.innerRadius * 2);
+        foreach (Collider2D c in colliders)
+        {
+            if (c == ownCollider)
+            {
+                return Reachable.IN_RANGE;
+            }
+        }
+
+        return Reachable.NOT_IN_RANGE;
     }
 }
